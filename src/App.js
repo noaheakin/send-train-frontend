e68907e5b496cef91e56ca9721651705e97928fb
noaheakin <noaheakin@gmail.com>
2020-11-25 00:50:52 -0800

    continued work on login/signup

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import SearchBar from './components/SearchBar';
 import CragsContainer from './components/SearchBar'
 import LogIn from './components/LogIn'
 import SignUp from './components/SignUp'
+import NavLink from 'react-router-dom/NavLink';
 
 const API = 'http://localhost:3000';
 
 class App extends Component {
+
+  state = {
+    user: null,
+    token: null
+  }
 
   handleSearchSubmit = e => {
     fetch(API + `/get_areas`)
@@ -17,12 +23,65 @@ class App extends Component {
     .then(console.log)
   }
 
-  handleLogIn = e => {
-
+  handleLogIn = info => {
+    console.log('login')
+    this.logInAuthFetch(info,'http://localhost:3000/login')
   }
 
   handleSignUp = e => {
+    e.preventDefault()
+    this.signUpAuthFetch(e,'http://localhost:3000/users')
+  }
 
+  logInAuthFetch = (info, request) => {  
+    console.log(info)
+    fetch(request,{
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: info.username,
+        password: info.password
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({user: data.user, token: data.token}, ()  =>{
+        console.log(this.state)
+        console.log(this.props)
+      }
+        )
+    })
+  }
+
+  signUpAuthFetch = (e, request) => {  
+    console.log(e)
+    debugger
+    fetch(request,{
+      method:'POST',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: e.target[0].value,
+        username: e.target[1].value,
+        password: e.target[2].value,
+        profile_pic: e.target[3].value,
+        bio: e.target[4].value,
+        location: e.target[5].value
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      this.setState({user: data.user, token: data.token}, ()  =>{
+        console.log(this.state)
+        console.log(this.props)
+      }
+        )
+    })
   }
 
   render() {
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 
-const LogIn = ({handleLogIn}) => {
-    return (
-        <div className='login'>
-            <form onSubmit={handleLogIn}>
-                <h3>Log In:</h3>
-                <label htmlFor="username">Username:</label>
-                <input type="text" id="username" name="username" required></input><br></br>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required></input><br></br><br></br>
-                <input type="submit" id="login-submit" name="login-submit"></input>
-                <Link to='/sign-up'>Not a User? Sign Up!</Link>
-            </form>
-        </div>
-    )
+class LogIn extends Component {
+
+    state = {
+        username: "",
+        password: ""
+    }
+
+    handleLogin = e => {
+        e.preventDefault()
+        this.props.handleLogIn(this.state)
+    }
+
+    handleChange = e => {
+        let {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    render () {
+        return (
+            <div className='login'>
+                <form onSubmit={this.handleLogin}>
+                    <h3>Log In:</h3>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange} required /><br></br>
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" name="password" value={this.state.password} onChange={this.handleChange} required /><br></br><br></br>
+                    <input type="submit" value="Log In"></input>
+                    <Link to='/sign-up'>Not a User? Sign Up!</Link>
+                </form>
+            </div>
+        )
+    } 
 }
 
 export default LogIn
