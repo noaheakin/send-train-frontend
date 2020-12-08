5cec06275ad58d878cfd730343696e40d1909e9e
noaheakin <noaheakin@gmail.com>
2020-12-08 11:23:09 -0800

    finished profile page styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -11,8 +11,8 @@ const Profile = ({user, displayEditPage}) => {
                     {user.profile_pic == "" ? <img className="profilePicBorder" height="300" width="300" src={process.env.PUBLIC_URL + '/images/default_profile.jpg'}/> : <img className="profilePicBorder" height="300" width="300" src={user.profile_pic}></img>}
                 </div>
                 </div>
-                <div class="profileInfo">
-                <div className="userInfoContainer" class="content">
+                <div class="middle aligned content">
+                <div className="profileInfo">
                     <div class="header"><h2>{user.username}</h2></div>
                     <div class="meta">
                         <span><em>{user.name}</em></span>
@@ -20,10 +20,10 @@ const Profile = ({user, displayEditPage}) => {
                     <div class="description">{user.bio}</div>
                     <div class="extra">
                         <p>{user.location}</p>
+                        <button class="ui orange button" onClick={() => displayEditPage()}>Edit Profile</button>
                     </div>
                 </div>
                 </div>
-                <button class="ui right floated orange button" onClick={() => displayEditPage()}>Edit Profile</button>
             </div>
         </div>
         </div>
