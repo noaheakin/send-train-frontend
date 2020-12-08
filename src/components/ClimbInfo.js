c4279e2b07e5f62e875cde938dd9c46782f49546
noaheakin <noaheakin@gmail.com>
2020-12-08 13:43:00 -0800

    styled display climb info page

--- src/components/ClimbInfo.js
+++ src/components/ClimbInfo.js
@@ -3,11 +3,24 @@ import StarRating from './StarRating'
 
 const ClimbInfo = ({ climb }) => {
     return (
+        
         <div className="selectedClimb">
-            <h2>{climb.name}</h2><h3><em>{climb.rating}</em></h3>
+        
+            <div className="climbImageContainer">
+            <img className="climbImage"src={climb.imgMedium} />
+            </div>
+            <div class="climbInfo">
+            <div class="ui card">
+            <div class="content">
+            <h2><div class="header">{climb.name}</div></h2>
+            <h3><div class="meta"><em>{climb.rating}</em></div></h3>
             <StarRating stars={climb.stars} starVotes={climb.starVotes}/>
-            <img src={climb.imgMedium} /><br></br>
-            <a href={climb.url} target="_blank">More info from MountainProject</a>
+            </div>
+            <div class="content">
+                <a href={climb.url} target="_blank">More info from MountainProject</a>
+            </div>
+            </div>
+        </div>
         </div>
     )
 }
